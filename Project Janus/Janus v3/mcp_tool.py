556c90b28634d3a56a5a8762577f5f6652fe380a
Snippet_cow <93636327+lifedebugging@users.noreply.github.com>
2026-04-11 13:41:12 +0530

    Create mcp_tool.py

---
+++ Project Janus/Janus v3/mcp_tool.py
@@ -0,0 +1,44 @@
+import asyncio
+from datetime import datetime
+from mcp.server import Server
+from mcp.types import Tool, TextContent
+from mcp.server.stdio import stdio_server
+
+app = Server("janus-engine")
+
+#the Discovery 
+@app.list_tools()
+async def list_my_tools():
+    return [
+        Tool(
+            name = "get_datetime",
+            description="Returns current date and time",
+            inputSchema={"types" : "object", "properties": {}},
+            )
+        ]
+
+# the Logic (The Switchboard)
+@app.call_tool()
+async def execute_tool(name, args):
+    if name == "get_datetime":
+        current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        return [TextContent(type="text", 
+                            text = f"The time is {current_datetime}"
+                           )]
+    else:
+    # Architect Tip: Always handle the "unknown tool" case
+        raise ValueError(f"Tool not found : {name}")
+
+#the Transport (The Pipes)
+async def main():
+    #1. (The stdio pipes)
+    async with stdio_server() as (read, write):
+        # 2. Start the brain (The server)
+        await app.run(read, 
+                      write, 
+                      app.create_initialization_options(), # These are the handshake settings
+                      )
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
