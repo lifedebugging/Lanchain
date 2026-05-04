3de502d86fd49b473b619ea815d5ef70980b83c0
Snippet_cow <93636327+lifedebugging@users.noreply.github.com>
2026-05-04 14:50:31 +0530

    Create client.py

---
+++ Project Janus/Janus v5/client.py
@@ -0,0 +1,30 @@
+import requests
+import time
+
+while True:
+    #input
+    query = {
+        "query": input("\n prompt: " )
+        }
+    
+    if query["query"] == "exit":
+        break
+
+    # POST to Janus
+    response = requests.post(url="http://127.0.0.1:8001/route", json=query, stream=True)
+    for chunk in response.iter_content(decode_unicode=True, chunk_size=None):
+        if chunk:
+            print(chunk, end="", flush=True)
+            time.sleep(0.05)  # 20ms delay between chunks for natural feel
+    
+        
+            
+    
+#parse response
+#final_response = response.json()["answer"]
+#no .json(). It's raw text chunks now.
+    #print answer
+    
+#iter_lines() returns bytes by default.
+
+    
